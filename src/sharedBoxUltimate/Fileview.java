78f322575ab0a0765f70c63ececd71560931e78c
Social-Master <97978160+Social-Master@users.noreply.github.com>
2022-01-19 15:15:23 +0100

    general look of the file view completed

--- src/sharedBoxUltimate/Fileview.java
+++ src/sharedBoxUltimate/Fileview.java
@@ -1,5 +1,5 @@
 /**
- * This class is the View class; it views the data, e.g. Files and Directories
+ * This class is the View class; it views the data, e.g. Files and Directories.
  */
 
 //package sharedBoxUltimate;
@@ -12,7 +12,8 @@ public class Fileview extends JFrame {
 
 	JFrame frame = null;
 	FlowLayout topLayout = new FlowLayout();
-	JTextArea directoryListingArea = null;
+	JTextArea directoryContentAreaPlaceholder = null;
+	//JTextArea userSettingsAreaPlaceholder = null;
 
 	public void go() {
 		frame = new JFrame("Shared-Box Ultimate");
@@ -20,6 +21,41 @@ public class Fileview extends JFrame {
 		frame.setSize(1000,500);
         frame.setLocation(300,300);
         frame.getContentPane();
+
+		final JPanel topPanel = new JPanel();
+		topPanel.setLayout(topLayout);
+		topLayout.setAlignment(FlowLayout.TRAILING);
+
+		JButton moveUpButton = new JButton("Move up directory");
+		JLabel filePathLabel = new JLabel("/user");
+
+		topPanel.add(filePathLabel);
+		topPanel.add(moveUpButton);
+		topPanel.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
+
+		directoryContentAreaPlaceholder = new JTextArea(25, 65);
+		directoryContentAreaPlaceholder.setEditable(false);
+
+		JPanel directoryContentAreaPanel = new JPanel();
+		directoryContentAreaPanel.add(directoryContentAreaPlaceholder);
+		directoryContentAreaPanel.setOpaque(true);
+
+		JPanel userSettingsPanel = new JPanel();
+		JButton editProfileButton = new JButton("Edit Profile");
+		JButton deleteProfileButton = new JButton("Delete Profile");
+		JButton createDepartmentButton = new JButton("Create Department");
+		JButton editDepartmentButton = new JButton("Edit Departments");
+		JButton deleteDepartmentsButton = new JButton("Delete Departments");
+		userSettingsPanel.setLayout(new BoxLayout(userSettingsPanel, BoxLayout.PAGE_AXIS));
+		userSettingsPanel.add(editProfileButton);
+		userSettingsPanel.add(deleteProfileButton);
+		userSettingsPanel.add(createDepartmentButton);
+		userSettingsPanel.add(editDepartmentButton);
+		userSettingsPanel.add(deleteDepartmentsButton);
+
+		frame.getContentPane().add(BorderLayout.NORTH, topPanel);
+		frame.getContentPane().add(BorderLayout.LINE_END, directoryContentAreaPanel);
+		frame.getContentPane().add(BorderLayout.LINE_START, userSettingsPanel);
 
 		frame.setVisible(true);
 	}
