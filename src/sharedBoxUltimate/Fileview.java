07e9748a8bdad78c3e06f4e2ce40e6b5689edc60
Social-Master <97978160+Social-Master@users.noreply.github.com>
2022-01-19 11:59:04 +0100

    Testing with syso

---
+++ src/sharedBoxUltimate/Fileview.java
@@ -0,0 +1,7 @@
+/**
+ * This is the GUI class for the file viewer.
+ */
+
+public class FileViewer {
+
+}
--- src/sharedBoxUltimate/Main.java
+++ src/sharedBoxUltimate/Main.java
@@ -1,5 +1,7 @@
 package sharedBoxUltimate;
 
 public class Main {
-	int a = 2;
+	public static void main (String[] args) {
+		System.out.println("Hello World!");
+	}
 }
