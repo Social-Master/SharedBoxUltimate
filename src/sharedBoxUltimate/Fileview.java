4ef50bfde4e8055ec943ff58527edc08e2f0cca1
Social-Master <97978160+Social-Master@users.noreply.github.com>
2022-01-21 19:26:53 +0100

    menubar added

--- src/sharedBoxUltimate/Fileview.java
+++ src/sharedBoxUltimate/Fileview.java
@@ -15,37 +15,59 @@ public class Fileview {
 
 	 JFrame frame = null;
 	 FlowLayout topLayout = new FlowLayout();
-	 JTextArea textArea = null;		// Placeholder for the real thing that views directory content.
-
+	 /*JTextArea textArea = null;		// Placeholder for the real thing that views directory content. */
+	 JMenuBar menuBar;
+	 JMenu menu;
+	 JMenuItem menuItem;
+	 
 	 public void fileviewGo() {
-
-		/**
-		 * These set the properties of the window, i.e. the title, its default size and its default placement.
-		 */
-
-		 frame = new JFrame("Shared-Box Ultimate");
-		 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		 frame.setSize(1000, 500);
-		 frame.setLocation(300, 300);
-		 frame.getContentPane();
-
+		 
 		 /**
-		  * This JTextArea is just a placeholder and will be replaced by a proper directory view
+		  * These set the properties of the window, i.e. the title, its default size and its default placement.
 		  */
+		  
+		  frame = new JFrame("Shared-Box Ultimate");
+		  frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		  frame.setSize(1000, 500);
+		  frame.setLocation(300, 300);
+		  frame.getContentPane();
+		  
+		  /**
+		   * This JTextArea is just a placeholder and will be replaced by a proper directory view
+		   */
+		  
+		  DefaultListModel<String> directoryContent = new DefaultListModel<>();
+		  directoryContent.addElement("Item0");
+		  directoryContent.addElement("Item1");
+		  directoryContent.addElement("Item2");
+		  directoryContent.addElement("Item3");
+		  JList<String> directoryContentList = new JList<>(directoryContent);
 
-		  textArea = new JTextArea(25, 65);
-		  textArea.append("Placeholder for directory content");
-		  textArea.setEditable(false);
-		  final JPanel textAreaPanel = new JPanel();
-		  textAreaPanel.add(textArea);
-		  textAreaPanel.setOpaque(true);
+		  //textArea = new JTextArea(25, 65);
+		  //textArea.append("Placeholder for directory content");
+		  //textArea.setEditable(false);
+		  final JPanel rightPanel = new JPanel();
+		  rightPanel.add(directoryContentList);
+		  rightPanel.setOpaque(true);
 
 		  final JPanel topPanel = new JPanel();
 		  topPanel.setLayout(topLayout);
 		  topLayout.setAlignment(FlowLayout.TRAILING);
 
+		  menuBar = new JMenuBar();
+		  menu = new JMenu("File");
+		  menu.setMnemonic(KeyEvent.VK_A);
+		  menuBar.add(menu);
+		  menuItem = new JMenuItem("See properties...", KeyEvent.VK_T);
+		  menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_1, ActionEvent.ALT_MASK));
+		  menu.add(menuItem);
+
 		  JButton moveUpButton = new JButton("Move up directory");
 		  JLabel filePathLabel = new JLabel("/user");	// This should display the current name of the directory.
+		  JButton reflectSelection = new JButton("Reflect Selection");
+		  topPanel.add(filePathLabel);
+		  topPanel.add(moveUpButton);
+		  topPanel.add(reflectSelection);
 
 		  /**
 		   * This deals with the settings of the current user. If an admin are logged in, the department related buttons will also appear; they are invisible to the user.
@@ -63,8 +85,10 @@ public class Fileview {
 		   userSettingsPanel.add(logoutButton);
 
 		   frame.getContentPane().add(BorderLayout.NORTH, topPanel);
-		   frame.getContentPane().add(BorderLayout.LINE_END, textAreaPanel);
+		   frame.getContentPane().add(BorderLayout.CENTER, rightPanel);
 		   frame.getContentPane().add(BorderLayout.LINE_START, userSettingsPanel);
+
+		   frame.setJMenuBar(menuBar);
 
 		   frame.setVisible(true);
 	 }
