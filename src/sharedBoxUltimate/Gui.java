d35160c4ea96c3774783b68b3de1313df642c565
Social-Master <97978160+Social-Master@users.noreply.github.com>
2022-01-24 13:13:10 +0100

    Gui.java

--- src/sharedBoxUltimate/Gui.java
+++ src/sharedBoxUltimate/Gui.java
@@ -4,7 +4,7 @@
  * Shared-Box Ultimate is a program that 
  */
 
-public class Main {
+public class Gui {
 	public static void main (String[] args) {
 		Loginview loginview = new Loginview();		// Creates a new Loginview window. More about this window, see the comments in Loginview.java
 		loginview.loginviewGo();
